fdf35804f6b3e874ae84e0cefe9c3618d31c1ab8
Prashant <prashant2003prashant@gmail.com>
2025-03-29 09:38:32 +0530

    added files

--- lib/app.dart
+++ lib/app.dart
@@ -30,3 +30,53 @@ class GalleryzeApp extends StatelessWidget {
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Todos',
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   final _future = Supabase.instance.client
+//       .from('todos')
+//       .select();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: FutureBuilder(
+//         future: _future,
+//         builder: (context, snapshot) {
+//           if (!snapshot.hasData) {
+//             return const Center(child: CircularProgressIndicator());
+//           }
+//           final todos = snapshot.data!;
+//           return ListView.builder(
+//             itemCount: todos.length,
+//             itemBuilder: ((context, index) {
+//               final todo = todos[index];
+//               return ListTile(
+//                 title: Text(todo['name']),
+//               );
+//             }),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
