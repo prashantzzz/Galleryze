ba8fbf17f20e3655bf7417f47fe5daa5564fa8a6
Prashant <prashant2003prashant@gmail.com>
2025-03-29 10:15:35 +0530

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,3 @@ import 'app.dart';
 void main() {
   runApp(const GalleryzeApp());
 }
-
-
-// import 'package:supabase_flutter/supabase_flutter.dart';
-
-// Future<void> main() async {
-//   await Supabase.initialize(
-//     url: 'https://bwvdoixtoagzpvorvymf.supabase.co',
-//     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dmRvaXh0b2FnenB2b3J2eW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMjI2MzYsImV4cCI6MjA1ODU5ODYzNn0.CmstnqO1NUcR6Em4R-8Jt7Eoc0LPZUaj_dIhDpOfYXc',
-//   );
-//   runApp(MyApp());
-// }
-        
